a05ce24dad95eb9696469043bc31d6b9ef189d60
Jorge Rocha <79389355+JorgeRochaA@users.noreply.github.com>
2022-06-08 15:55:18 -0600

    feat: add navbar (#1)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Home from "./components/Home";
 import "./App.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
