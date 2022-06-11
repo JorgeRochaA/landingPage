2a72da087d54aefb020fb232210407c75a52b930
JorgeRochaA <jor2019.cr@gmail.com>
2022-06-10 19:28:52 -0600

    feat: add blog section

--- src/components/BlogSection.jsx
+++ src/components/BlogSection.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import BlogCard from "../components/BlogCard";
+import CardBlogData from "./cardBlogData.json";
 import "../styles/BlogSection/BlogSection.scss";
 function BlogSection() {
   return (
@@ -14,6 +16,19 @@ function BlogSection() {
         <div className="blog_button_container">
           <div className="blog_button"></div>
         </div>
+      </div>
+      <div className="blog_cards_container">
+        {CardBlogData.map((cardBlogData, index) => {
+          return (
+            <BlogCard
+              key={index}
+              image={cardBlogData.image}
+              title={cardBlogData.title}
+              description={cardBlogData.description}
+              button_name={cardBlogData.button_name}
+            />
+          );
+        })}
       </div>
     </div>
   );
