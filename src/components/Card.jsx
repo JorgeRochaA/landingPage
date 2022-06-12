60f6e0721527e5123ae97af2b7bd4a9459dd080a
JorgeRochaA <jor2019.cr@gmail.com>
2022-06-12 17:31:50 -0600

    style: change card style

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,10 @@ import "../styles/Card/Card.scss";
 function Card({ url, title, description }) {
   return (
     <div className="card">
-      <img src={url} alt={title} />
+      <div className="circle">
+        {" "}
+        <img src={url} alt={title} />
+      </div>
       <div className="card_title">{title}</div>
       <div className="card_description">{description}</div>
     </div>
